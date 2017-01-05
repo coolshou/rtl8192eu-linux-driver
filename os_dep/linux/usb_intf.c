fb2c0a84c57751e6989a3328560972687dc3a1c3
coolshou <coolshou2060@gmail.com>
2017-01-05 11:57:20 +0800

    add DWA-131 C1

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -213,7 +213,8 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0x818B,0xff,0xff,0xff),.driver_info = RTL8192E},/* Default ID */
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0x818C,0xff,0xff,0xff),.driver_info = RTL8192E},/* Default ID */
 	/*=== Customer ID ===*/
-	{USB_DEVICE(0x2001, 0x3319),.driver_info = RTL8192E}, /* D-Link - DWA-131 */
+	{USB_DEVICE(0x2001, 0x3319),.driver_info = RTL8192E}, /* D-Link - DWA-131 E1*/
+	{USB_DEVICE(0x2001, 0x3312),.driver_info = RTL8192E}, /* D-Link - DWA-131 C1 - Alibaba*/
 	/*===TPLINK ID===========*/
 	{USB_DEVICE(0x2357, 0x0107),.driver_info = RTL8192E}, /* TP-Link - Cameo */
 	{USB_DEVICE(0x2357, 0x0108),.driver_info = RTL8192E}, /* TP-Link - Cameo */
