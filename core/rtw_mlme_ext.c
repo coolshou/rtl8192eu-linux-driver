4e386538481dfcab0c5be06a833c4bc451d1ae16
coolshou <jimmy_yeh@alphanetworks.com>
2025-11-20 18:05:40 +0800

    add {}

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -2323,13 +2323,15 @@ unsigned int OnAuth(_adapter *padapter, union recv_frame *precv_frame)
 				if (pstat->tbtx_enable)
 					pstapriv->tbtx_asoc_list_cnt--;
 				#endif
-				if (pstat->expire_to > 0)
+				if (pstat->expire_to > 0){
 					;/* TODO: STA re_auth within expire_to */
+				}
 			}
 			_exit_critical_bh(&pstapriv->asoc_list_lock, &irqL);
 
-			if (seq == 1)
+			if (seq == 1){
 				; /* TODO: STA re_auth and auth timeout */
+			}
 
 		}
 	}
