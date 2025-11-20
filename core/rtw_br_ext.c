764ee8aea78d49fc8272ba7d61eb9bccfcdff581
coolshou <jimmy_yeh@alphanetworks.com>
2025-11-20 18:22:02 +0800

    remove ipx

--- core/rtw_br_ext.c
+++ core/rtw_br_ext.c
@@ -17,12 +17,10 @@
 #ifdef __KERNEL__
 	#include <linux/if_arp.h>
 	#include <net/ip.h>
-	#include <linux/version.h>
-	#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0)
+	#include <linux/version.h>	
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0))	
 	#include <net/ipx.h>
-	#else
-	#include "net/ipx.h"
-	#endif
+#endif		
 	#include <linux/atalk.h>
 	#include <linux/udp.h>
 	#include <linux/if_pppox.h>
@@ -62,8 +60,10 @@
 
 #define NAT25_IPV4		01
 #define NAT25_IPV6		02
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0))
 #define NAT25_IPX		03
 #define NAT25_APPLE		04
+#endif
 #define NAT25_PPPOE		05
 
 #define RTL_RELAY_TAG_LEN (ETH_ALEN)
@@ -174,6 +174,7 @@ static __inline__ void __nat25_generate_ipv4_network_addr(unsigned char *network
 }
 
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0))
 static __inline__ void __nat25_generate_ipx_network_addr_with_node(unsigned char *networkAddr,
 		unsigned int *ipxNetAddr, unsigned char *ipxNodeAddr)
 {
@@ -183,7 +184,6 @@ static __inline__ void __nat25_generate_ipx_network_addr_with_node(unsigned char
 	memcpy(networkAddr + 1, (unsigned char *)ipxNetAddr, 4);
 	memcpy(networkAddr + 5, ipxNodeAddr, 6);
 }
-
 
 static __inline__ void __nat25_generate_ipx_network_addr_with_socket(unsigned char *networkAddr,
 		unsigned int *ipxNetAddr, unsigned short *ipxSocketAddr)
@@ -205,7 +205,7 @@ static __inline__ void __nat25_generate_apple_network_addr(unsigned char *networ
 	memcpy(networkAddr + 1, (unsigned char *)network, 2);
 	networkAddr[3] = *node;
 }
-
+#endif
 
 static __inline__ void __nat25_generate_pppoe_network_addr(unsigned char *networkAddr,
 		unsigned char *ac_mac, unsigned short *sid)
@@ -335,6 +335,7 @@ static __inline__ int __nat25_network_hash(unsigned char *networkAddr)
 		x = networkAddr[7] ^ networkAddr[8] ^ networkAddr[9] ^ networkAddr[10];
 
 		return x & (NAT25_HASH_SIZE - 1);
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0))		
 	} else if (networkAddr[0] == NAT25_IPX) {
 		unsigned long x;
 
@@ -348,6 +349,7 @@ static __inline__ int __nat25_network_hash(unsigned char *networkAddr)
 		x = networkAddr[1] ^ networkAddr[2] ^ networkAddr[3];
 
 		return x & (NAT25_HASH_SIZE - 1);
+#endif		
 	} else if (networkAddr[0] == NAT25_PPPOE) {
 		unsigned long x;
 
@@ -894,6 +896,8 @@ int nat25_db_handle(_adapter *priv, struct sk_buff *skb, int method)
 		}
 	}
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0))
+
 	/*---------------------------------------------------*/
 	/*         Handle IPX and Apple Talk frame          */
 	/*---------------------------------------------------*/
@@ -1114,6 +1118,8 @@ int nat25_db_handle(_adapter *priv, struct sk_buff *skb, int method)
 
 		return -1;
 	}
+
+#endif
 
 	/*---------------------------------------------------*/
 	/*                Handle PPPoE frame                */
