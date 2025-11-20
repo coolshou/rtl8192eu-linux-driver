bce2dc60b64942405e1dabe647e1d015f1393f06
coolshou <jimmy_yeh@alphanetworks.com>
2025-11-20 17:23:30 +0800

    test

--- include/linux/old_wireless.h
+++ include/linux/old_wireless.h
@@ -19,19 +19,22 @@
 /***************************** INCLUDES *****************************/
 
 #if 0
-	#include <linux/types.h>		/* for __u* and __s* typedefs */
-	#include <linux/socket.h>		/* for "struct sockaddr" et al	*/
-	#include <linux/if.h>			/* for IFNAMSIZ and co... */
+#include <linux/types.h>  /* for __u* and __s* typedefs */
+#include <linux/socket.h> /* for "struct sockaddr" et al	*/
+#include <linux/if.h>	  /* for IFNAMSIZ and co... */
 #else
-	#define __user
-	/* typedef uint16_t	__u16; */
-	#include <sys/socket.h>			/* for "struct sockaddr" et al	*/
-	#include <net/if.h>			/* for IFNAMSIZ and co... */
+#ifndef __user
+#define __user
+#endif
+/* typedef uint16_t	__u16; */
+#include <sys/socket.h> /* for "struct sockaddr" et al	*/
+#include <net/if.h>		/* for IFNAMSIZ and co... */
 #endif
 
 /****************************** TYPES ******************************/
 #ifdef CONFIG_COMPAT
-struct compat_iw_point {
+struct compat_iw_point
+{
 	compat_caddr_t pointer;
 	__u16 length;
 	__u16 flags;
@@ -42,12 +45,12 @@ struct compat_iw_point {
  *	For all data larger than 16 octets, we need to use a
  *	pointer to memory allocated in user space.
  */
-struct	iw_point {
-	void __user	*pointer;	/* Pointer to the data  (in user space) */
-	__u16		length;		/* number of fields or size in bytes */
-	__u16		flags;		/* Optional params */
+struct iw_point
+{
+	void __user *pointer; /* Pointer to the data  (in user space) */
+	__u16 length;		  /* number of fields or size in bytes */
+	__u16 flags;		  /* Optional params */
 };
-
 
 /* ------------------------ IOCTL REQUEST ------------------------ */
 /*
@@ -60,13 +63,14 @@ struct	iw_point {
  * You should check this when increasing the structures defined
  * above in this file...
  */
-union	iwreq_data {
+union iwreq_data
+{
 	/* Config - generic */
-	char		name[IFNAMSIZ];
+	char name[IFNAMSIZ];
 	/* Name : used to verify the presence of  wireless extensions.
 	 * Name of the protocol/provider... */
 
-	struct iw_point	data;		/* Other large parameters */
+	struct iw_point data; /* Other large parameters */
 };
 
 /*
@@ -75,13 +79,15 @@ union	iwreq_data {
  * convenience...
  * Do I need to remind you about structure size (32 octets) ?
  */
-struct	iwreq {
-	union {
-		char	ifrn_name[IFNAMSIZ];	/* if name, e.g. "eth0" */
+struct iwreq
+{
+	union
+	{
+		char ifrn_name[IFNAMSIZ]; /* if name, e.g. "eth0" */
 	} ifr_ifrn;
 
 	/* Data part (defined just above) */
-	union	iwreq_data	u;
+	union iwreq_data u;
 };
 
-#endif	/* _LINUX_WIRELESS_H */
+#endif /* _LINUX_WIRELESS_H */
